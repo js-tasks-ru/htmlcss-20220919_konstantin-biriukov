80dbca5159fb877d96bc07703c4a59575a92271b
Konstantin Biriukov <kostya-leon@mail.ru>
2022-11-24 22:27:13 -0600

    scss, postcss

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    syntax: "postcss-scss",
+    map: {inline: true},
+    plugins: [
+        require("postcss-nested"),
+        require("postcss-import"),
+        require("postcss-advanced-variables"),
+        require("autoprefixer"),
+        require("cssnano")
+    ]
+}
