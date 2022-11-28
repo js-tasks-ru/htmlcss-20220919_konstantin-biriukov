84732d855ca123d5c5d1b2416122c1d777fc6251
Konstantin Biriukov <kostya-leon@mail.ru>
2022-11-28 10:38:00 -0800

    switch from using main.css to styles.css(minified)

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,9 @@ module.exports = {
     syntax: "postcss-scss",
     map: {inline: true},
     plugins: [
-        require("postcss-nested"),
-        require("postcss-import"),
-        require("postcss-advanced-variables"),
+        // require("postcss-nested"),
+        // require("postcss-import"),
+        // require("postcss-advanced-variables"),
         require("autoprefixer"),
         require("cssnano")
     ]
